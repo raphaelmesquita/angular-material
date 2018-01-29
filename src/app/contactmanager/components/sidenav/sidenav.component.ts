c3459a1187aa9dcf1fb3dfaa51061e0ffaf7836c
Raphael Lima <rlima@gaveainvest.com.br>
2018-01-29 14:25:37 -0200

    Implementação de mediaMatcher

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  private mediaMatcher: MediaQueryList =
+    matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+
+  constructor(zone: NgZone) {
+    this.mediaMatcher.addListener(mql =>
+      zone.run(() => this.mediaMatcher = mql));
+  }
 
   ngOnInit() {
   }
 
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
+  }
 }
