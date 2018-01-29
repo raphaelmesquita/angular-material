02303cd7917b673a6b789c09e21cbac7fb89fbbc
Raphael Lima <rlima@gaveainvest.com.br>
2018-01-29 19:11:13 -0200

    Adição de ação quando a notificação é clicada

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,16 +14,31 @@ export class ToolbarComponent implements OnInit {
 
   @Output() toggleSideNav = new EventEmitter();
 
-  constructor(private _dialog: MatDialog) { }
+  constructor(
+    private _dialog: MatDialog,
+    private _snackBar: MatSnackBar,
+    private _router: Router) { }
 
   ngOnInit() {
   }
 
   openAddContactDialog(): void {
     let dialogRef = this._dialog.open(NewContactDialogComponent, { width: "450px" });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: User) => {
       console.log("The dialog was closed", result);
+
+      if (result) {
+        this.openSnackBar("Contact added", "Navigate")
+          .onAction().subscribe(() => {
+            this._router.navigate(["/contactmanager", result.id]);
+          });
+      }
     });
   }
 
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this._snackBar.open(message, action, {
+      duration: 5000,
+    });
+  }
 }
