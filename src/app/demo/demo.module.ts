7ed8e7e0fcf0b88f98a796807b2c5f10d44f5024
Raphael Lima <rlima@gaveainvest.com.br>
2018-01-29 11:53:03 -0200

    Inclusão do @angular/flex-layout

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
+import { MaterialModule } from '../shared/material.module';
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
-import { MaterialModule } from '../shared/material.module';
 import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
     CommonModule,
     DemoRoutingModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   declarations: [ButtonsComponent, FlexboxComponent]
 })
